3c535eb930fc3ad0cdc621e5e84383c2adb45baa
Tom Le <tomle.zaku@gmail.com>
2024-01-10 14:12:02 +0800

    feat: add keymap and support image for terminal

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
+-- Example for configuring Neovim to load user-installed installed Lua rocks:
+--$ luarocks --local --lua-version=5.1 install magick
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
+
 -- Load Modules:
 require("core")
